b142788f5b0ff86755756fcffe6a364fb8053360
Rashaad Washington <rwashington12@my.apsu.edu>
2022-04-23 21:31:32 -0500

    added powerpointviewer but might remove it

--- ScribeSharp/MainWindow.xaml.cs
+++ ScribeSharp/MainWindow.xaml.cs
@@ -65,6 +65,7 @@ namespace ScribeSharp
         {
             ClassroomPage classroom_page = new ClassroomPage(this);
             this.Content = classroom_page;
+            //template on how to open page
 
         }
 
